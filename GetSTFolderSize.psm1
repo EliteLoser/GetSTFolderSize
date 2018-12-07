eba0f4b0c58f5e2b97c4d70a108b386dd922d66d
Joakim Svendsen <joakimbs@gmail.com>
2018-12-07 21:47:36 +0100

    Add exclude dir/file feature
    There will be six more returned properties with -ExcludeDirectory or -ExcludeFile in use for Copied/Skipped and Dir/File/Bytes in combination. Keeping it completely backwards compatible as long as you don't use the new parameters (that didn't exist in the older version, so yeah).

--- GetSTFolderSize.psm1
+++ GetSTFolderSize.psm1
@@ -48,6 +48,12 @@ function Get-STFolderSize {
         Number of threads used when falling back to robocopy, or with -RoboOnly.
         Default: 16 (gave the fastest results during my testing).
 
+    .PARAMETER ExcludeDirectory
+        Names and paths for directories to exclude, as supported by the RoboCopy.exe /XD syntax.
+        
+    .PARAMETER ExcludeFile
+        Names/paths/wildcards for files to exclude, as supported by the RoboCopy.exe /XF syntax.
+        
     .EXAMPLE
         Import-Module .\Get-FolderSize.psm1
         PS C:\> 'C:\Windows', 'E:\temp' | Get-STFolderSize
@@ -73,20 +79,29 @@ function Get-STFolderSize {
     [CmdletBinding(DefaultParameterSetName = "Path")]
     param(
         [Parameter(ParameterSetName = "Path",
-                   Mandatory = $true,
-                   ValueFromPipeline = $true,
-                   ValueFromPipelineByPropertyName = $true,
+                   Mandatory = $True,
+                   ValueFromPipeline = $True,
+                   ValueFromPipelineByPropertyName = $True,
                    Position = 0)]
             [Alias('Name', 'FullName')]
-            [string[]] $Path,
-        [int] $Precision = 4,
-        [switch] $RoboOnly,
-        [switch] $ComOnly,
+            [String[]] $Path,
+        [System.Int32] $Precision = 4,
+        [Switch] $RoboOnly,
+        [Switch] $ComOnly,
         [Parameter(ParameterSetName = "LiteralPath",
                    Mandatory = $true,
-                   Position = 0)] [string[]] $LiteralPath,
-        [ValidateRange(1, 128)] [byte] $RoboThreadCount = 16)
-    begin {
+                   Position = 0)] [String[]] $LiteralPath,
+        [ValidateRange(1, 128)] [Byte] $RoboThreadCount = 16,
+        [String[]] $ExcludeDirectory = @(),
+        [String[]] $ExcludeFile = @())
+    
+    Begin {
+    
+        $DefaultProperties = 'Path', 'TotalBytes', 'TotalMBytes', 'TotalGBytes', 'DirCount', 'FileCount',
+            'DirFailed', 'FileFailed', 'TimeElapsed', 'StartedTime', 'EndedTime'
+        if ($ExcludeDirectory.Count -gt 0 -or $ExcludeFile.Count -gt 0) {
+            $DefaultProperties += @("CopiedDirCount", "CopiedFileCount", "CopiedBytes", "SkippedDirCount", "SkippedFileCount", "SkippedBytes")
+        }
         if ($RoboOnly -and $ComOnly) {
             Write-Error -Message "You can't use both -ComOnly and -RoboOnly. Default is COM with a fallback to robocopy." -ErrorAction Stop
         }
@@ -97,8 +112,8 @@ function Get-STFolderSize {
             [CmdletBinding()]
             param(
                 # Paths to report size, file count, dir count, etc. for.
-                [string[]] $Path,
-                [int] $Precision = 4)
+                [String[]] $Path,
+                [System.Int32] $Precision = 4)
             begin {
                 if (-not (Get-Command -Name robocopy -ErrorAction SilentlyContinue)) {
                     Write-Warning -Message "Fallback to robocopy failed because robocopy.exe could not be found. Path '$p'. $([datetime]::Now)."
@@ -109,32 +124,44 @@ function Get-STFolderSize {
                 foreach ($p in $Path) {
                     Write-Verbose -Message "Processing path '$p' with Get-RoboFolderSizeInternal. $([datetime]::Now)."
                     $RoboCopyArgs = @("/L","/S","/NJH","/BYTES","/FP","/NC","/NDL","/TS","/XJ","/R:0","/W:0","/MT:$RoboThreadCount")
-                    [datetime] $StartedTime = [datetime]::Now
-                    [string] $Summary = robocopy $p NULL $RoboCopyArgs | Select-Object -Last 8
-                    [datetime] $EndedTime = [datetime]::Now
-                    [string] $DefaultIgnored = '(?:\s+[\-\d]+){3}'
-                    [regex] $HeaderRegex = '\s+Total\s*Copied\s+Skipped\s+Mismatch\s+FAILED\s+Extras'
-                    [regex] $DirLineRegex = "Dirs\s*:\s*(?<DirCount>\d+)$DefaultIgnored\s+(?<DirFailed>\d+)\s+[\-\d]+"
-                    [regex] $FileLineRegex = "Files\s*:\s*(?<FileCount>\d+)$DefaultIgnored\s+(?<FileFailed>\d+)\s+[\-\d]+"
-                    [regex] $BytesLineRegex = "Bytes\s*:\s*(?<ByteCount>\d+)$DefaultIgnored\s+(?<BytesFailed>\d+)\s+[\-\d]+"
-                    [regex] $TimeLineRegex = "Times\s*:\s*.*"
-                    [regex] $EndedLineRegex = "Ended\s*:\s*(?<EndedTime>.+)"
+                    if ($ExcludeDirectory.Count -gt 0) {
+                        $RoboCopyArgs += @(@("/XD") + @($ExcludeDirectory))
+                    }
+                    if ($ExcludeFile.Count -gt 0) {
+                        $RoboCopyArgs += @(@("/XF") + @($ExcludeFile))
+                    }
+                    [DateTime] $StartedTime = [DateTime]::Now
+                    [String] $Summary = robocopy $p NULL $RoboCopyArgs | Select-Object -Last 8
+                    [DateTime] $EndedTime = [DateTime]::Now
+                    #[String] $DefaultIgnored = '(?:\s+[\-\d]+){3}'
+                    [Regex] $HeaderRegex = '\s+Total\s*Copied\s+Skipped\s+Mismatch\s+FAILED\s+Extras'
+                    [Regex] $DirLineRegex = "Dirs\s*:\s*(?<DirCount>\d+)(?<CopiedDirCount>\s+[\-\d]+)(?<SkippedDirCount>\s+[\-\d]+)(?:\s+[\-\d]+)\s+(?<DirFailed>\d+)\s+[\-\d]+"
+                    [Regex] $FileLineRegex = "Files\s*:\s*(?<FileCount>\d+)(?<CopiedFileCount>\s+[\-\d]+)(?<SkippedFileCount>\s+[\-\d]+)(?:\s+[\-\d]+)\s+(?<FileFailed>\d+)\s+[\-\d]+"
+                    [Regex] $BytesLineRegex = "Bytes\s*:\s*(?<ByteCount>\d+)(?<CopiedBytes>\s+[\-\d]+)(?<SkippedBytes>\s+[\-\d]+)(?:\s+[\-\d]+)\s+(?<BytesFailed>\d+)\s+[\-\d]+"
+                    [Regex] $TimeLineRegex = "Times\s*:\s*.*"
+                    [Regex] $EndedLineRegex = "Ended\s*:\s*(?<EndedTime>.+)"
                     if ($Summary -match "$HeaderRegex\s+$DirLineRegex\s+$FileLineRegex\s+$BytesLineRegex\s+$TimeLineRegex\s+$EndedLineRegex") {
                         New-Object PSObject -Property @{
                             Path = $p
-                            TotalBytes = [decimal] $Matches['ByteCount']
-                            TotalMBytes = [math]::Round(([decimal] $Matches['ByteCount'] / 1MB), $Precision)
-                            TotalGBytes = [math]::Round(([decimal] $Matches['ByteCount'] / 1GB), $Precision)
-                            BytesFailed = [decimal] $Matches['BytesFailed']
-                            DirCount = [decimal] $Matches['DirCount']
-                            FileCount = [decimal] $Matches['FileCount']
-                            DirFailed = [decimal] $Matches['DirFailed']
-                            FileFailed  = [decimal] $Matches['FileFailed']
-                            TimeElapsed = [math]::Round([decimal] ($EndedTime - $StartedTime).TotalSeconds, $Precision)
+                            TotalBytes = [Decimal] $Matches['ByteCount']
+                            TotalMBytes = [Math]::Round(([Decimal] $Matches['ByteCount'] / 1MB), $Precision)
+                            TotalGBytes = [Math]::Round(([Decimal] $Matches['ByteCount'] / 1GB), $Precision)
+                            BytesFailed = [Decimal] $Matches['BytesFailed']
+                            DirCount = [Decimal] $Matches['DirCount']
+                            FileCount = [Decimal] $Matches['FileCount']
+                            DirFailed = [Decimal] $Matches['DirFailed']
+                            FileFailed  = [Decimal] $Matches['FileFailed']
+                            TimeElapsed = [Math]::Round([Decimal] ($EndedTime - $StartedTime).TotalSeconds, $Precision)
                             StartedTime = $StartedTime
                             EndedTime   = $EndedTime
+                            CopiedDirCount = [Decimal] $Matches['CopiedDirCount']
+                            CopiedFileCount = [Decimal] $Matches['CopiedFileCount']
+                            CopiedBytes = [Decimal] $Matches['CopiedBytes']
+                            SkippedDirCount = [Decimal] $Matches['SkippedDirCount']
+                            SkippedFileCount = [Decimal] $Matches['SkippedFileCount']
+                            SkippedBytes = [Decimal] $Matches['SkippedBytes']
 
-                        } | Select-Object -Property Path, TotalBytes, TotalMBytes, TotalGBytes, DirCount, FileCount, DirFailed, FileFailed, TimeElapsed, StartedTime, EndedTime
+                        } | Select-Object -Property $DefaultProperties
                     }
                     else {
                         Write-Warning -Message "Path '$p' output from robocopy was not in an expected format."
@@ -142,8 +169,10 @@ function Get-STFolderSize {
                 }
             }
         }
+    
     }
-    process {
+    
+    Process {
         if ($PSCmdlet.ParameterSetName -eq "Path") {
             $Paths = @(Resolve-Path -Path $Path | Select-Object -ExpandProperty ProviderPath -ErrorAction SilentlyContinue)
         }
@@ -151,7 +180,7 @@ function Get-STFolderSize {
             $Paths = @(Get-Item -LiteralPath $LiteralPath | Select-Object -ExpandProperty FullName -ErrorAction SilentlyContinue)
         }
         foreach ($p in $Paths) {
-            Write-Verbose -Message "Processing path '$p'. $([datetime]::Now)."
+            Write-Verbose -Message "Processing path '$p'. $([DateTime]::Now)."
             if (-not (Test-Path -LiteralPath $p -PathType Container)) {
                 Write-Warning -Message "$p does not exist or is a file and not a directory. Skipping."
                 continue
@@ -163,9 +192,9 @@ function Get-STFolderSize {
             }
             $ErrorActionPreference = 'Stop'
             try {
-                $StartFSOTime = [datetime]::Now
+                $StartFSOTime = [DateTime]::Now
                 $TotalBytes = $FSO.GetFolder($p).Size
-                $EndFSOTime = [datetime]::Now
+                $EndFSOTime = [DateTime]::Now
                 if ($null -eq $TotalBytes) {
                     if (-not $ComOnly) {
                         Get-RoboFolderSizeInternal -Path $p -Precision $Precision
@@ -193,25 +222,30 @@ function Get-STFolderSize {
             $ErrorActionPreference = 'Continue'
             New-Object PSObject -Property @{
                 Path = $p
-                TotalBytes = [decimal] $TotalBytes
-                TotalMBytes = [math]::Round(([decimal] $TotalBytes / 1MB), $Precision)
-                TotalGBytes = [math]::Round(([decimal] $TotalBytes / 1GB), $Precision)
+                TotalBytes = [Decimal] $TotalBytes
+                TotalMBytes = [Math]::Round(([Decimal] $TotalBytes / 1MB), $Precision)
+                TotalGBytes = [Math]::Round(([Decimal] $TotalBytes / 1GB), $Precision)
                 BytesFailed = $null
                 DirCount = $null
                 FileCount = $null
                 DirFailed = $null
                 FileFailed  = $null
-                TimeElapsed = [math]::Round(([decimal] ($EndFSOTime - $StartFSOTime).TotalSeconds), $Precision)
+                TimeElapsed = [Math]::Round(([Decimal] ($EndFSOTime - $StartFSOTime).TotalSeconds), $Precision)
                 StartedTime = $StartFSOTime
                 EndedTime = $EndFSOTime
-            } | Select-Object -Property Path, TotalBytes, TotalMBytes, TotalGBytes, DirCount, FileCount, DirFailed, FileFailed, TimeElapsed, StartedTime, EndedTime
+            } | Select-Object -Property $DefaultProperties
         }
     }
-    end {
+    
+    End {
+        
         if (-not $RoboOnly) {
-            [void][System.Runtime.Interopservices.Marshal]::ReleaseComObject($FSO)
+            [Void] [System.Runtime.Interopservices.Marshal]::ReleaseComObject($FSO)
         }
-        [gc]::Collect()
-        [gc]::WaitForPendingFinalizers()
+        
+        [System.GC]::Collect()
+        #[System.GC]::WaitForPendingFinalizers()
+    
     }
+
 }
